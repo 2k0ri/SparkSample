8702a4a11f06acd1e68e2b09d9ca4121fa8170a3
2k0ri <esc13245@gmail.com>
2016-04-17 23:24:00 +0900

    initial commit

---
+++ src/main/scala/org/k2i/SparkSampleApp.scala
@@ -0,0 +1,27 @@
+package org.k2i
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.SQLContext
+import com.databricks.spark.avro._
+
+/**
+  * Hello world!
+  *
+  */
+object SparkSampleApp {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("SparkSampleApp")
+    val sc = new SparkContext(conf)
+    val sqlContext = new SQLContext(sc)
+
+    val avroFile = "/usr/local/opt/apache-spark/libexec/examples/src/main/resources/users.avro"
+    val df = sqlContext.read.avro(avroFile)
+
+    case class Person(name: String, favorite_color: String, favorite_numbers: Array[Integer])
+
+    val ds = df.as[Person]
+
+    ds.filter(_.favorite_numbers.length > 1).show
+
+  }
+}
---
+++ src/test/scala/org/k2i/SparkSampleAppTest.scala
@@ -0,0 +1,17 @@
+package org.k2i
+
+import org.junit._
+import Assert._
+
+@Test
+class SparkSampleAppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
